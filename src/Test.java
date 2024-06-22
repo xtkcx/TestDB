84b6ab1618fc8c930fb9e63708d9bbd501a5ae12
xtkcx <1117take@gmail.com>
2024-06-23 03:09:48 +0900

    version10

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@ public class Test {
 		dao.select("taro", "123");
 		dao.selectAll();
 		dao.selectByName("taro");
+		dao.selectByPassword("123");
+		dao.updateUserNameByUserName("taro", "saburo");
 	}
 }
